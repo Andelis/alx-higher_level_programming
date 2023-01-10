95712c3c29596a8f576398d659e8549fa72405ad
Etieno Andem <etienoandem@gmail.com>
2023-01-09 19:02:58 -0600

    file updated

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" Defines an empty class BaseGeometry. """
+"""Defines an empty class BaseGeometry."""
+
+
 class BaseGeometry:
-    """ Represent base geometry """
+    """Represent base geometry."""
     pass
