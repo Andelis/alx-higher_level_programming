b3b8b8b5cefaab158f0dc23a123aa7d13cd7dbeb
Etieno Andem <etienoandem@gmail.com>
2023-01-10 19:19:51 -0600

    correction

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -10,5 +10,5 @@ def append_write(filename="", text=""):
     Returns:
         The number of characters appended.
     """
-    with open(filename, “b”, encoding="utf-8") as f:
+    with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
